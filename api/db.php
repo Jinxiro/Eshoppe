6a7e5e48d33454f6c8b1c90c7cabd8a358ba129d
unknown <jammilitar@gmail.com>
2024-04-18 09:49:25 +0800

    first commit

---
+++ api/db.php
@@ -0,0 +1,20 @@
+<?php
+
+function getDb(){
+    $host = "localhost";
+    $dbname = "eshopdb";
+    $user = "eshopdb";
+    $password = "cvHCQy*.v/UB/UqK";
+    
+    $dsn = "mysql:
+            host=$host;
+            dbname=$dbname;
+            user=$user;
+            password=$password;
+    ";
+
+    $conn = new PDO($dsn);
+    return $conn;
+}
+
+getDb();
---
+++ api/user.php
@@ -0,0 +1,72 @@
+<?php
+require("./db.php");
+
+$action = $_GET['action'] ?? null;
+
+if ($action == 'login'){
+    login();
+}
+if ($action == 'check'){
+    check();
+}
+
+
+function generateToken($length = 32) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?\+';
+    $token = '';
+    for ($i = 0; $i < $length; $i++) {
+        $token .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $token;
+}
+
+function login() {
+    $payload = file_get_contents("php://input");
+    $payload = json_decode($payload);
+
+    $conn = getDb();
+    $query = $conn->query("SELECT * FROM `user`
+    WHERE 
+    `username` LIKE '$payload->username' AND
+    `password` LIKE '$payload->password'
+    ");
+
+     $results = $query->fetch(PDO::FETCH_ASSOC);
+     
+     
+     if($results) {
+         http_response_code(200);
+         $token = generateToken();
+
+         $updateQuery = $conn->query("UPDATE `user` SET `token` = '$token' 
+         WHERE `username` = '$payload->username'");
+        
+         echo $token;
+
+     }else{
+         http_response_code(400);
+         echo "invalid credential";
+     }
+     
+    }
+
+function check() {
+    $payload = file_get_contents("php://input");
+    $payload = json_decode($payload);
+    
+    $conn = getDb();
+    $query = $conn->query("SELECT * FROM `user` 
+    WHERE 
+    `token` LIKE '$payload->token'
+    ");
+
+    $results = $query->fetch(PDO::FETCH_ASSOC);
+
+if($results){
+    http_response_code(200);
+    echo "Valid";
+}else{
+    http_response_code(400);
+    echo "Invalid"; 
+}
+}
